076ae2b226d8b6c3cff274bc2c8a2a4b1e23d2fd
Giordanny <giorkryer@gmail.com>
2026-03-02 19:11:13 -0300

    fullstack todo-list with Rails API and React

---
+++ backend/app/controllers/api/todos_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  class TodosController < ApplicationController
+    # GET /api/todos
+    def index
+      @todos = ::Todo.all
+      render json: @todos
+    end
+
+    # POST /api/todos
+    def create
+      @todo = ::Todo.new(todo_params)
+      if @todo.save
+        render json: @todo, status: :created
+      else
+        render json: @todo.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /api/todos/:id
+    def destroy
+      @todo = ::Todo.find(params[:id])
+      @todo.destroy
+      head :no_content
+    end
+
+    # PATCH /api/todos/:id/update_completed
+    def update_completed
+      @todo = ::Todo.find(params[:id])
+      if @todo.update(todo_params)
+        render json: @todo
+      else
+        render json: @todo.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def todo_params
+      # Garante que o Rails aceite os dados enviados pelo seu React
+      params.require(:todo).permit(:todo_name, :completed)
+    end
+  end
+end
